9d7192d22a88f44c2aba109a36e9a145d922471f
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-06-10 13:26:53 +0900

    feat: Add type selecting example

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # js-and-typescript-for-react
-리액트에서 빈번히 사용되는 자바스크립트와 타입스크립트 기초 문법 연습.<br>
-유튜버 조교행님 의 [React 시작 전에 알아야 할 JavaScript, TypeScript](https://youtube.com/playlist?list=PLLDrd87CR4wiLY6phShHItvEyzetCDX9F) 강의를 바탕으로 진행합니다.
+
+리액트를 위한 자바스크립트와 타입스크립트 기초 문법 연습.
+유튜버 코딩앙마 님의 [TypeScript 강좌](https://youtube.com/playlist?list=PLZKTXPmaJk8KhKQ_BILr1JKCJbR0EGlx0) 강의를 바탕으로 진행합니다.
---
+++ out/index.js
@@ -0,0 +1,27 @@
+"use strict";
+// * 1. 매개변수의 타입 지정하기
+// 명시한 타입과 일치하지 않는 인수로 함수를 호출하면 아래의 에러 메세지 발생.
+// Argument of type 'string' is not assignable to parameter of type 'number'.
+function multiply(num1, num2) {
+    console.log(num1 * num2);
+}
+multiply(2, 3);
+// multiply('1', 2);
+// * 1-1. 매개변수로 객체(요소가 숫자인 배열) 받기
+function showItems(arr) {
+    arr.forEach(e => console.log(e));
+}
+showItems([1, 2, 3, 4]);
+/** 자바스크립트 방식: 매개변수의 타입을 지정할 수 없음.
+ *
+ * function multiply(num1, num2) {
+    console.log(num1 * num2);
+}
+multiply(2, 3);
+
+function showItems(arr) {
+  arr.forEach(e => console.log(e));
+}
+showItems([1, 2, 3, 4]);
+ *
+ */
